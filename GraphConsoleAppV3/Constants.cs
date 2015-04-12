babfa7a0848b8ff147418147e8720622525b1b4b
dstrockis <dastrock@microsoft.com>
2015-04-11 21:00:42 -0700

    Switching to new AAD endpoint

--- GraphConsoleAppV3/Constants.cs
+++ GraphConsoleAppV3/Constants.cs
@@ -7,7 +7,7 @@
         public const string ClientId = "118473c2-7619-46e3-a8e4-6da8d5f56e12";
         public const string ClientSecret = "hOrJ0r0TZ4GQ3obp+vk3FZ7JBVP+TX353kNo6QwNq7Q=";
         public const string ClientIdForUserAuthn = "66133929-66a4-4edc-aaee-13b04b03207d";
-        public const string AuthString = "https://login.windows.net/" + TenantName;
+        public const string AuthString = "https://login.microsoftonline.com/" + TenantName;
         public const string ResourceUrl = "https://graph.windows.net";
     }
 }
