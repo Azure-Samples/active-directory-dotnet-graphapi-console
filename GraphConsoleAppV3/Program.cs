9b74344fca21986fe4bf2849ffc376077538b701
calvli <calvli@microsoft.com>
2017-03-31 10:21:25 -0700

    adding formatting to print request IDs and date for OData error. (#44)

--- GraphConsoleAppV3/Program.cs
+++ GraphConsoleAppV3/Program.cs
@@ -8,6 +8,9 @@ using System.Net;
 
 namespace GraphConsoleAppV3
 {
+    using System.Data.Services.Client;
+    using System.Web.Script.Serialization;
+
     public class Program
     {
 
@@ -53,14 +56,28 @@ namespace GraphConsoleAppV3
         public static string ExtractErrorMessage(Exception exception)
         {
             List<string> errorMessages = new List<string>();
+
+
             string tabs = "\n";
             while (exception != null)
             {
+                string requestIdLabel = "requestId";
+                if (exception is DataServiceClientException &&
+                    exception.Message.Contains(requestIdLabel))
+                {
+                    Dictionary<string, object> odataError =
+                        new JavaScriptSerializer().Deserialize<Dictionary<string, object>>(exception.Message);
+                    odataError = (Dictionary<string, object>)odataError["odata.error"];
+                    errorMessages.Insert(0, "\nRequest ID: " + odataError[requestIdLabel]);
+                    errorMessages.Insert(1, "Date: " + odataError["date"]);
+                }
+
                 tabs += "    ";
                 errorMessages.Add(tabs + exception.Message);
                 exception = exception.InnerException;
             }
-            return string.Join("-\n", errorMessages);
+
+            return string.Join("\n", errorMessages);
         }
 
         public static void WriteError(string output, params object[] args)
